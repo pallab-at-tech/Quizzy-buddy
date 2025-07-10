3b1bff79836bd621b5b4ec80c6b45f9d9c24fe90
pallab-at-tech <pallab861774@gmail.com>
2025-07-10 20:22:35 +0530

    till now responsive

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,14 +8,14 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <section >
-            
-            <div className='min-h-screen bg-gradient-to-b from-[#c5d4f7] to-[#1546ba]'>
+
+            <div className='min-h-screen lg:block grid grid-rows-[100px_150px_1fr]  bg-gradient-to-b from-[#c5d4f7] to-[#1546ba]'>
 
                 <div>
-                    <ul className='grid grid-cols-[1fr_200px_200px] py-4'>
+                    <ul className='grid md:grid-cols-[1fr_200px_200px] grid-cols-3 py-6'>
                         <li>Logo</li>
                         <li>
-                            <Link to={"/sign-up"}  className='px-4 py-2 text-white text-base rounded-4xl font-semibold bg-[#010e49] hover:bg-[#031461] hover:scale-105 hover:px-[16px] transition-all duration-150 cursor-pointer'>Sign Up</Link>
+                            <Link to={"/sign-up"} className='px-4 py-2 text-white text-base rounded-4xl font-semibold bg-[#010e49] hover:bg-[#031461] hover:scale-105 hover:px-[16px] transition-all duration-150 cursor-pointer'>Sign Up</Link>
                         </li>
                         <li>
                             <Link to={"/sign-in"} className='px-4 py-2 text-white text-base rounded-4xl font-semibold bg-[#010e49] hover:bg-[#031461] hover:scale-105 hover:px-[16px] transition-all duration-150 cursor-pointer'>Sign in</Link>
@@ -23,13 +23,13 @@ const Home = () => {
                     </ul>
                 </div>
 
-                <div className='flex items-center justify-around mt-8 mb-12 text-base text-[#010e49] font-bold'>
+                <div className='md:flex grid grid-cols-2 grid-rows-2 place-items-center items-center justify-around mt-8 mb-12 text-base text-[#010e49] font-bold'>
 
                     <div className='flex gap-1 items-center'>
                         <div className='text-[#df6f00] '>
                             <IoStar size={20} />
                         </div>
-                        <p>host Quiz</p>
+                        <p>host your Quiz</p>
                     </div>
 
                     <div className='flex gap-1 items-center'>
@@ -55,25 +55,29 @@ const Home = () => {
 
                 </div>
 
-                <div className='w-full flex items-center flex-col text-[#000415]'>
-                    <h1 className='font-bold text-[80px] relative top-[50px] right-[55px]'>Welcome to ,</h1>
-                    <div className='text-[80px] font-bold flex items-center'>Quizzy budd <p className='text-[110px] font-semibold'>y</p> </div>
-                    <h1 className='relative top-[-33px] right-[-60px] text-[#06187e] font-semibold text-lg'>
-                        "Where Challenge. Learn. Repeat."
-                    </h1>
-                    <button className='px-3 py-3 text-white text-base rounded-2xl font-semibold bg-[#010e49] hover:bg-[#031461] hover:scale-105 hover:px-[13px] transition-all duration-150 cursor-pointer flex items-center gap-1'>
-                        <PiShootingStarFill size={20} />
-                        <p>start journey</p>
-                    </button>
+                <div>
 
-                </div>
+                    <div className='w-full flex items-center flex-col text-[#000415]'>
+                        <h1 className='font-bold md:text-[80px] text-[45px] top-[20px] right-6 relative md:top-[50px] md:right-[55px]'>Welcome to ,</h1>
+                        <div className='md:text-[80px] text-[45px] font-bold flex items-center'>Quizzy budd <p className='md:text-[110px] font-semibold'>y</p> </div>
+                        <h1 className='relative md:top-[-33px] md:right-[-60px] text-[#06187e] font-semibold md:text-lg text-sm'>
+                            "Where Challenge. Learn. Repeat."
+                        </h1>
+                        <button className='px-3 py-3 lg:mt-0 md:mt-4 mt-6 text-white text-base rounded-2xl font-semibold bg-[#010e49] hover:bg-[#031461] hover:scale-105 hover:px-[13px] transition-all duration-150 cursor-pointer flex items-center gap-1'>
+                            <PiShootingStarFill size={20} />
+                            <Link to={"/sign-up"}>start journey</Link>
+                        </button>
 
-                <div className='absolute top-[48%] right-[10%] opacity-[40%] rotate-[10deg]'>
-                    <img src={backimg1} alt="" className='h-[300px]' />
-                </div>
+                    </div>
 
-                <div className='absolute top-[48%] left-[13%] scale-x-[-1] opacity-[40%] rotate-[358deg]'>
-                    <img src={backimg2} alt="" className='h-[300px]' />
+                    <div className='absolute lg:block hidden top-[48%] right-[10%] opacity-[40%] rotate-[10deg]'>
+                        <img src={backimg1} alt="" className='h-[300px]' />
+                    </div>
+
+                    <div className='absolute lg:block hidden top-[48%] left-[13%] scale-x-[-1] opacity-[40%] rotate-[358deg]'>
+                        <img src={backimg2} alt="" className='h-[300px]' />
+                    </div>
+
                 </div>
 
             </div>
--- frontend/src/pages/SignInPage.jsx
+++ frontend/src/pages/SignInPage.jsx
@@ -13,7 +13,7 @@ const SignInPage = () => {
 
     const navigate = useNavigate()
     const location = useLocation()
-  
+
 
 
     const handleChange = (e) => {
@@ -31,9 +31,9 @@ const SignInPage = () => {
 
 
     return (
-        <section className={`min-w-screen max-w-screen min-h-screen max-h-screen bg-[#d1cece] overflow-hidden relative`}>
+        <section className={`min-w-screen max-w-screen min-h-screen max-h-screen grid lg:grid-cols-[30%_1fr] bg-[#e1dede] overflow-hidden`}>
 
-            <div className={`absolute left-0 top-0 bottom-0 w-[30%] rounded-r-2xl bg-[#1c45a4] flex flex-col justify-around px-16 items-center`}>
+            <div className={`lg:flex hidden w-full rounded-r-2xl bg-[#1c45a4]  flex-col justify-around px-16 items-center`}>
 
                 <div className='text-[#e7e7e7] text-4xl font-bold text-center'>
                     <p>Don't have an</p>
@@ -46,29 +46,38 @@ const SignInPage = () => {
             </div>
 
 
-            <div className={`absolute right-0 top-0 bottom-0 w-[70%]`}>
+            <div className={`md:px-0 px-8 md:-mt-0 -mt-[150px]`}>
                 <form className={`flex flex-col items-center justify-center h-full w-full gap-1`}>
 
-                    <h1 className='text-3xl font-bold text-[#000727] my-2'>Welcome Your Account</h1>
+                    <h1 className='md:text-3xl text-2xl font-bold text-[#000727] my-2'>Welcome Your Account</h1>
 
 
                     <div className='group'>
                         <p className='font-semibold group-hover:scale-y-105 transition-all duration-500 group-hover:-translate-y-1'>Email : </p>
-                        <input type="email" onChange={handleChange} name='email' value={data.email} required className='bg-[#b2b8de] rounded w-[320px] h-8 text-base outline-none p-2 mt-1 text-[#100f0f]' />
+                        <input type="email" onChange={handleChange} name='email' value={data.email} required className='bg-[#b2b8de] rounded md:w-[320px] w-[250px] h-8 text-base outline-none p-2 mt-1 text-[#100f0f]' />
                     </div>
 
                     <div className='group'>
                         <p className='font-semibold group-hover:scale-y-105 transition-all duration-500 group-hover:-translate-y-1'>Password : </p>
-                        <input type="text" onChange={handleChange} name='password' value={data.password} required className='bg-[#b2b8de] w-[320px]  h-8 text-base outline-none p-2 mt-1 text-[#100f0f]' />
+                        <input type="text" onChange={handleChange} name='password' value={data.password} required className='bg-[#b2b8de] md:w-[320px] w-[250px]  h-8 text-base outline-none p-2 mt-1 text-[#100f0f]' />
                     </div>
 
 
                     <div className='flex flex-col gap-1'>
-                        <button disabled={!valid} className={`p-2  w-[320px] bg-[#1c45a4] text-[#d1cece]  mt-2 rounded  font-semibold`}>Login</button>
-                        <Link className='text-[#000727] text-sm pr-6 pb-2'>Forgot Password ?</Link>
+                        <button disabled={!valid} className={`p-2  md:w-[320px] w-[250px] bg-[#1c45a4] text-[#d1cece]  mt-2 rounded  font-semibold`}>Login</button>
+                        <Link className='text-[#1c45a4] text-sm pr-6'>Forgot Password ?</Link>
+
+                        <div className='lg:hidden flex text-sm gap-1'>
+                            <p className='text-[#1c45a4]'>Don't have account ?</p>
+                            <Link to={"/sign-up"}>sign up</Link>
+                        </div>
                     </div>
 
+
+
                 </form>
+
+
             </div>
 
 

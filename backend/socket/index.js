311c476379fae63e57ad6f2f7cecc538372d48a3
pallab-at-tech <pallab861774@gmail.com>
2025-07-13 00:54:15 +0530

    socker error resolved

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const server = createServer(app)

c71588fabf3269c11257ff92b56e7c947a7db1bf
pallab-at-tech <pallab861774@gmail.com>
2025-10-11 20:45:02 +0530

    profile page responsive

--- frontend/src/Layout/Profile.jsx
+++ frontend/src/Layout/Profile.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import avatar from "../assets/avatar.png"
 import { useSelector } from 'react-redux'
 import { LuSchool } from "react-icons/lu";
@@ -9,23 +8,27 @@ const Profile = () => {
 
   const user = useSelector(state => state.user)
 
-  return (
-    <div className='pt-6 px-4 bg-[#f9f8f8] pb-0 mb-0'>
+  // console.log("user",user)
 
-      <div className='rounded-t-xl shadow-lg min-h-[180px] max-h-[180px] overflow-hidden relative'>
+  return (
+    <div className='pt-3 px-3  bg-[#f9f8f8] pb-0 mb-0'>
+
+      {/* outer background */}
+      <div className='rounded-t-xl shadow-lg overflow-hidden relative'>
 
         <div className='m-5 p-2 cursor-pointer bg-[#ffff] text-blue-600 w-fit rounded-full absolute right-0 top-0'>
           <MdOutlineModeEdit size={25} />
         </div>
 
-        <img src={banner} alt="" className='bg-cover' />
+        <img src={banner} alt="" className='bg-cover object-cover h-[80px] sm:h-[100px] w-full' />
 
       </div>
 
-      <div className='rounded-b-xl shadow-lg py-4 flex gap-1 items-center relative bg-[#fcfcfc]'>
+      {/* profile section */}
+      <div className='rounded-b-xl shadow-lg py-4 flex flex-col sm:flex-row gap-1 items-start sm:items-center relative bg-[#fcfcfc] px-4'>
 
         {/* profile */}
-        <div className='px-8 py-2'>
+        <div className='px-0 sm:px-8 py-2'>
           <div className='rounded-full border-4 overflow-hidden border-blue-800'>
             <img src={avatar} alt="" className='w-[110px] h-[110x] rounded-full' />
           </div>
@@ -38,7 +41,7 @@ const Profile = () => {
             <h2 className='relative bottom-1 text-base text-[#2c2c2c]'>userName</h2>
           </div>
 
-          <div className='flex items-center gap-1'>
+          <div className='flex items-center gap-1 pt-2 pb-1'>
             <div className='p-1 rounded-md border border-[#c4c4c4]'>
               <LuSchool size={18} />
             </div>
@@ -47,7 +50,7 @@ const Profile = () => {
 
         </div>
 
-        <div className='text-white bg-blue-600 flex items-center gap-2 px-3 py-2 rounded-full absolute right-4 bottom-10 cursor-pointer'>
+        <div className='text-white bg-blue-600 flex items-center gap-2 px-3 py-2 rounded-full absolute right-4 bottom-[60px] sm:bottom-10 cursor-pointer'>
 
           <div>
             <MdOutlineModeEdit />
@@ -59,14 +62,14 @@ const Profile = () => {
 
       </div>
 
-      <div className='mt-5 text-lg rounded-t-xl px-6 pt-4 bg-[#fcfcfc] ' style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 1px 0px 12px' }}>
+      {/* other section */}
+      <div className='mt-3 text-lg rounded-t-xl px-6 py-4 bg-[#fcfcfc] ' style={{ boxShadow: 'rgba(0, 0, 0, 0.1) 1px 0px 12px' }}>
 
         <p className='font-bold'>About</p>
 
         <div className='text-sm max-w-[70%]'>hi i am MERN Stack developer.</div>
 
         <p className='text-blue-700 text-base font-semibold cursor-pointer'>Add About</p>
-
 
       </div>
 

2ef77d2fddf4d5a843c72d84b48e386194243388
pallab-at-tech <pallab861774@gmail.com>
2025-12-30 02:01:49 +0530

    final logo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="flex items-center gap-2 cursor-pointer font-sans select-none group">
+    <div className="flex items-center gap-1.5 cursor-pointer font-sans select-none group">
 
       {/* Icon Badge */}
-      <div style={{ boxShadow: "inset rgb(128 128 128 / 41%) 1px 1px 6px , inset rgb(38 57 159) 1px 1px 6px"  }} className={`flex items-center justify-center sm:w-9 sm:h-9 w-7 h-7 rounded-lg
+      <div style={{ boxShadow: "inset rgb(124 124 243 / 51%) 1px 1px 6px , inset rgb(34 51 139) 1px 1px 4px 1px"  }} className={`flex items-center justify-center sm:w-9 sm:h-9 w-7 h-7 rounded-lg
                       bg-gradient-to-br from-[#3e4eff] to-[#2753cb]
                       border-[2px] border-blue-800
                       group-hover:scale-105 transition-transform duration-300
                       `}
       >
         <span className="sm:text-[1.6rem] text-[1.1rem] drop-shadow-sm text-white font-bold">
-          <img src="/app-logo.png" alt="" className="w-[22px] h-[22px]"/>
+          <img src="/app-logo.png" alt="" className="w-[18px] h-[18px] custom-sm:w-[22px] custom-sm:h-[22px]"/>
         </span>
       </div>
 

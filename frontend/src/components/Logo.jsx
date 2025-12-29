76e9b11e2b0010e9a2f7545ad5978fcd110153a7
pallab-at-tech <pallab861774@gmail.com>
2025-12-30 01:47:36 +0530

    remove unnessary files

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -5,14 +5,14 @@ const Logo = () => {
     <div className="flex items-center gap-2 cursor-pointer font-sans select-none group">
 
       {/* Icon Badge */}
-      <div style={{ boxShadow: "inset rgb(128 128 128 / 41%) 1px 1px 6px , inset rgb(66 91 225) 1px 1px 6px"  }} className={`flex items-center justify-center sm:w-9 sm:h-9 w-7 h-7 rounded-lg
+      <div style={{ boxShadow: "inset rgb(128 128 128 / 41%) 1px 1px 6px , inset rgb(38 57 159) 1px 1px 6px"  }} className={`flex items-center justify-center sm:w-9 sm:h-9 w-7 h-7 rounded-lg
                       bg-gradient-to-br from-[#3e4eff] to-[#2753cb]
                       border-[2px] border-blue-800
                       group-hover:scale-105 transition-transform duration-300
                       `}
       >
         <span className="sm:text-[1.6rem] text-[1.1rem] drop-shadow-sm text-white font-bold">
-          <img src="/owl.png" alt="" className="w-[22px] h-[22px]"/>
+          <img src="/app-logo.png" alt="" className="w-[22px] h-[22px]"/>
         </span>
       </div>
 
